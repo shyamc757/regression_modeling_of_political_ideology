4b5a267106be59b513a59d92fa602abb9cde9743
Neel Agarwal <neelagarwal98@gmail.com>
2024-09-14 15:07:28 -0400

    updated main and lemmatizer

--- lemmatizer.py
+++ lemmatizer.py
@@ -1,10 +1,53 @@
 import nltk
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
+# nltk.data.path.append('/Users/neelagarwal/IDrive Sync/Cloud-Drive/For Cloud/MS DS UoR/Semester 3/Data Science Practicum/Kaggle/training_data.csv')
+# nltk.download('popular')
+
+
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet
+
+import cleantext
+import demoji
+import re
+# import wordnet
 
 lemmatizer = WordNetLemmatizer()
 
+def tweet_cleaner(input_text):
+     
+    # # removal of b's in the beginning 
+    # if (input_text.startswith("b'") or input_text.startswith("b\"")) and input_text.endswith("'"):
+    #     input_text = input_text[2:-1]
+        
+    # if (input_text.startswith("b'") or input_text.startswith("b\"")) and (not(input_text.endswith("'"))):
+    #     input_text = input_text[2:]
+
+    input_text = demoji.replace(input_text,"") # removal of emoji
+    input_text = " ".join(input_text.split()) # removal of extra spaces
+    input_text = re.sub(r'https?:\/\/.*[\r\n]*', '', input_text) # removal of url startign with 'http'
+
+    # input_text_list = input_text.split()
+    if not input_text:
+        return ""
+    input_text = cleantext.clean(input_text,
+                                    punct = True # removal of punctuation
+                                    )
+    stop_words = set(stopwords.words('english'))
+    input_text = " ".join([word for word in input_text.split() if word not in stop_words]) # removal of stopwords
+    input_text = " ".join([word for word in input_text.split() if len(word) >= 3]) # removal of words less than 3 characters
+    return(input_text)
+
 ##Tags the words in the tweets
+
 def nltk_tag_to_wordnet_tag(nltk_tag):
     if nltk_tag.startswith('J'):
         return(wordnet.ADJ)
@@ -20,6 +63,9 @@ def nltk_tag_to_wordnet_tag(nltk_tag):
 ##Lemmatizes the words in tweets and returns the cleaned and lemmatized tweet
 def lemmatize_tweet(tweet):
     #tokenize the tweet and find the POS tag for each token
+    # return if empty string
+    if not tweet:
+        return "" 
     tweet = tweet_cleaner(tweet) #tweet_cleaner() will be the function you will write
     nltk_tagged = nltk.pos_tag(nltk.word_tokenize(tweet))  
     #tuple of (token, wordnet_tag)
